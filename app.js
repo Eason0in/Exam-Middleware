07b52c2c1451c56e9534875701a6ca13d00ee4cd
Eason <leoling127@gmail.com>
2019-05-18 23:12:35 +0800

    Add view pages

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
@@ -11,28 +12,31 @@ const requestData = (req, res, next) => {
 }
 
 app.use(requestData)
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 // 列出全部 Todo
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+  res.render('index', { message: '列出全部Todo' })
 })
 
 // 新增一筆 Todo 頁面
 app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
+  res.render('index', { message: '新增 Todo 頁面' })
 })
 
 // 顯示一筆 Todo 的詳細內容
 app.get('/:id', (req, res) => {
-  res.send('顯示一筆 Todo')
+  res.render('index', { message: '顯示一筆Todo' })
 })
 
 // 新增一筆  Todo
 app.post('/', (req, res) => {
-  res.send('新增一筆  Todo')
+  res.render('index', { message: 'POST-新增一筆 Todo' })
 })
 
-app.delete('/:id/delete', (req, res) => {
-  res.send('刪除 Todo')
+app.post('/:id/delete', (req, res) => {
+  res.render('index', { message: 'POST-刪除 Todo' })
 })
 
 app.listen(port, () => {
